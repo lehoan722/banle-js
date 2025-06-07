77f8fa29c81f8c606f48346d7cb63c7d209dbfa9
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2025-06-07 20:56:13 +0700

    Update popupHanghoa.js

--- public/scripts/popupHanghoa.js
+++ public/scripts/popupHanghoa.js
@@ -252,9 +252,10 @@ document.addEventListener("keydown", function (e) {
     if (cauhinh && cauhinh.style.display !== "none") {
       cauhinh.style.display = "none";
     }
-  }
-  const popupBangDanhMuc = document.getElementById("popupBangDanhMuc");
+    const popupBangDanhMuc = document.getElementById("popupBangDanhMuc");
     if (popupBangDanhMuc && popupBangDanhMuc.style.display !== "none") {
       popupBangDanhMuc.style.display = "none";
     }
+  }
+  
 });
