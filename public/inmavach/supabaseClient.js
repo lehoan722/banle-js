177353e71dd518fc3cce312cb0c8e32a3cffb743
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2025-06-18 09:39:25 +0700

    Update supabaseClient.js

--- public/inmavach/supabaseClient.js
+++ public/inmavach/supabaseClient.js
@@ -2,6 +2,7 @@
 const { createClient } = supabase;
 
 window.supabase = createClient(
-  'https://YOUR-SUPABASE-URL.supabase.co',
-  'YOUR-ANON-KEY'
+  'https://rddjrmbyftlcvrgzlyby.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZGpybWJ5ZnRsY3ZyZ3pseWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NjU4MDQsImV4cCI6MjA2MjM0MTgwNH0.-0xtqxn6b9OBz4unTTvJ4klxizWhHa1iSuYGm7cOYTM'
 );
+
