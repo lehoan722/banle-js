9fa916da617871e19c89eaeed574d99a79eab9b7
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2025-06-18 09:55:20 +0700

    Update supabaseClient.js

--- public/inmavach/supabaseClient.js
+++ public/inmavach/supabaseClient.js
@@ -1,8 +1,10 @@
 // supabaseClient.js
-const { createClient } = supabase;
 
-window.supabase = createClient(
-  'https://rddjrmbyftlcvrgzlyby.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZGpybWJ5ZnRsY3ZyZ3pseWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NjU4MDQsImV4cCI6MjA2MjM0MTgwNH0.-0xtqxn6b9OBz4unTTvJ4klxizWhHa1iSuYGm7cOYTM'
-);
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabaseUrl = 'https://rddjrmbyftlcvrgzlyby.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZGpybWJ5ZnRsY3ZyZ3pseWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NjU4MDQsImV4cCI6MjA2MjM0MTgwNH0.-0xtqxn6b9OBz4unTTvJ4klxizWhHa1iSuYGm7cOYTM';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
 
