cd6ab04225edf5ffafdc12b1f2b3ed8c4f64208f
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2025-06-21 16:23:57 +0700

    Update supabaseClient.js

--- public/scripts/supabaseClient.js
+++ public/scripts/supabaseClient.js
@@ -1,6 +1,1 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-export const supabase = createClient(
-  "https://rddjrmbyftlcvrgzlyby.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZGpybWJ5ZnRsY3ZyZ3pseWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NjU4MDQsImV4cCI6MjA2MjM0MTgwNH0.-0xtqxn6b9OBz4unTTvJ4klxizWhHa1iSuYGm7cOYTM" // 🔒 dùng role `anon`
-);
+export const supabase = window.supabase;
