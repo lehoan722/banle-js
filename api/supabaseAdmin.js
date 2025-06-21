d926d0b2a56c2dd3c808f05767a2981eb040efa9
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2025-06-21 09:04:33 +0700

    Delete api/supabaseAdmin.js

--- api/supabaseAdmin.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  "https://rddjrmbyftlcvrgzlyby.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZGpybWJ5ZnRsY3ZyZ3pseWJ5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0Njc2NTgwNCwiZXhwIjoyMDYyMzQxODA0fQ.6UBSL-2jW7Qj73W8PEKOtIeDcGldbCMwpHn1He0MfhM"
-)
