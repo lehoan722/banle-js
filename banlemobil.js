50b2024e7cef0e5681a1aa3be11845051c113a13
dang le hoan <lehoan722@gmail.com>
2025-08-05 13:13:14 +0000

    Edit banlemobil.js

--- banlemobil.js
+++ banlemobil.js
@@ -3,7 +3,7 @@
 // ====== 1. Khởi tạo Supabase Client ======
 const supabaseUrl = 'https://rddjrmbyftlcvrgzlyby.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZGpybWJ5ZnRsY3ZyZ3pseWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NjU4MDQsImV4cCI6MjA2MjM0MTgwNH0.-0xtqxn6b9OBz4unTTvJ4klxizWhHa1iSuYGm7cOYTM';
-const supabase = window.supabase = window.supabase || supabaseJs?.createClient?.(supabaseUrl, supabaseKey) || createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase || supabase.createClient(supabaseUrl, supabaseKey);
 
 // ====== 2. Biến toàn cục ======
 let dsSanPham = [];
