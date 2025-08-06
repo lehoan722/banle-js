ad4a31e915874504548ece3b7a19786a3e681d91
dang le hoan <lehoan722@gmail.com>
2025-08-06 04:05:37 +0000

    Edit banlemobil.js

--- scripts/banlemobil.js
+++ scripts/banlemobil.js
@@ -1,4 +1,7 @@
 // ===== 1. Khởi tạo Supabase Client =====
+
+import { tinhKhuyenMai } from './khuyenmai.js';
+
 const supabaseUrl = 'https://rddjrmbyftlcvrgzlyby.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZGpybWJ5ZnRsY3ZyZ3pseWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NjU4MDQsImV4cCI6MjA2MjM0MTgwNH0.-0xtqxn6b9OBz4unTTvJ4klxizWhHa1iSuYGm7cOYTM';
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
