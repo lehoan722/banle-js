06da8b7c466f12d8d5e1ae1e112260bc8c5250af
dang le hoan <lehoan722@gmail.com>
2025-08-12 10:24:18 +0000

    Edit baocaoxnt14.js

--- scripts/baocaoxnt14.js
+++ scripts/baocaoxnt14.js
@@ -212,25 +212,19 @@ window.taiBaoCaoXNT = async function () {
 
     hotInstance = new Handsontable(container, {
         data: hotData,
-        columns: columns,        
+        columns: columns,
+        colHeaders: columns.map(col => col.title),
         rowHeaders: true,
         width: '100%',
-        
+        height: 550,
         copyPaste: {
             copyColumnHeaders: true,   // bật copy kèm tiêu đề cột
             // (tuỳ chọn) rowsLimit: 100000,
             // (tuỳ chọn) columnsLimit: 1000
         },
         licenseKey: 'non-commercial-and-evaluation',
-
+        
         stretchH: 'all',
-        height: '97%',            // cho phép resizeHotHeight() điều khiển       
-        colHeaders(index) {         // (nếu muốn 2 dòng) vẫn giữ header 2 dòng
-            const t = (columns[index]?.title || '').trim();
-            return t.replace(/\s+/g, '<br>');
-        },
-
-
         manualColumnResize: true,
         readOnly: true,
         hiddenColumns: { columns: [15], indicators: false },
