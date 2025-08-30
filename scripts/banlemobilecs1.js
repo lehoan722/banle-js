b0ea1d9825ba6b959ec8a3acd51bf6c58bbb2fbd
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2025-08-31 05:16:02 +0700

    Update banlemobilecs1.js

--- scripts/banlemobilecs1.js
+++ scripts/banlemobilecs1.js
@@ -1,10 +1,9 @@
 // ===== 1. Khởi tạo Supabase Client ===== 
 
 import { tinhKhuyenMai } from './khuyenmai.js';
+// Dùng chung client đã tạo trong HTML (đã đăng nhập)
+const _supabase = window.supabase;
 
-const supabaseUrl = 'https://rddjrmbyftlcvrgzlyby.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZGpybWJ5ZnRsY3ZyZ3pseWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NjU4MDQsImV4cCI6MjA2MjM0MTgwNH0.-0xtqxn6b9OBz4unTTvJ4klxizWhHa1iSuYGm7cOYTM';
-const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 // ===== 2. Biến toàn cục =====
 let dsSanPham = [];
