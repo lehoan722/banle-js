81acb1cc053eafae3d418fff0ea2f168ce7bc5d1
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2025-09-04 23:52:30 +0700

    Update sohoadon.js

--- scripts/sohoadon.js
+++ scripts/sohoadon.js
@@ -24,6 +24,8 @@ export async function capNhatSoHoaDonTuDong() {
         let loai = "";
         if (pathname.includes("banle")) {
             loai = diadiem === "cs1" ? "bancs1" : "bancs2";
+        } else if (pathname.includes("banlenv")) {
+            loai = diadiem === "cs1" ? "banlenvcs1" : "banlenvcs2";
         } else if (pathname.includes("nhapmoi")) {
             loai = diadiem === "cs1" ? "nmcs1" : "nmcs2";
         } else if (pathname.includes("nhapdoi")) {
