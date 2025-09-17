d181575a5212b52401f749ced82e4bca86150498
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2025-09-17 10:34:18 +0700

    Update soundBeep.js

--- scripts/soundBeep.js
+++ scripts/soundBeep.js
@@ -44,7 +44,7 @@ export function playSuccessBeep() {
 
 // “tút” — dài, trầm hơn
 export function playWaitSizeBeep() {
-  beep(1500, 200, "sine", 0.7);
+  beep(1200, 240, "sine", 0.7);
 }
 
 // Tuỳ chọn: beep cảnh báo (âm vuông, hơi “gắt” hơn)
