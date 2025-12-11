4e03fea9c008015d8798a0227a9eb8ce93ff1971
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2025-12-11 18:27:58 +0700

    Update supabaseClient.js

--- scripts/supabaseClient.js
+++ scripts/supabaseClient.js
@@ -1,13 +1,13 @@
 
 
-// scripts/supabaseClient.js
+
+// supabaseClient.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.48.0/+esm";
 
 const SUPABASE_URL = "https://rddjrmbyftlcvrgzlyby.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZGpybWJ5ZnRsY3ZyZ3pseWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NjU4MDQsImV4cCI6MjA2MjM0MTgwNH0.-0xtqxn6b9OBz4unTTvJ4klxizWhHa1iSuYGm7cOYTM";
 
-// Nếu trên window chưa có supabase client thì tạo mới
-// Nếu authModule đã tạo rồi thì dùng lại đúng client đó (có session đăng nhập)
+// Nếu chưa có client trên window thì mới tạo
 if (
   !window.supabase ||
   !window.supabase.auth ||
@@ -16,5 +16,5 @@ if (
   window.supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 }
 
-// Tất cả file khác import đều dùng chung 1 client này
+// Tất cả các file khác import sẽ dùng đúng 1 client này
 export const supabase = window.supabase;
