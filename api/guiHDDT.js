a2dc046cb2406e39b79922cad509e52d8fcc84fe
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2026-01-07 21:38:41 +0700

    Update guiHDDT.js

--- api/guiHDDT.js
+++ api/guiHDDT.js
@@ -122,7 +122,7 @@ export default async function handler(req, res) {
     }
 
     return res.status(200).json({
-      // message: 'Gửi hóa đơn thành công',
+      ok: true,
       detail: hoaDonData
     });
 
