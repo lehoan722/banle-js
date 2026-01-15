ed1ae2fc00359683d43dbdd40837cc230c6de0eb
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2026-01-15 16:11:26 +0700

    Update viettelInvoice.js

--- scripts/viettelInvoice.js
+++ scripts/viettelInvoice.js
@@ -17,8 +17,8 @@ function taoDuLieuHoaDon(hoadon, chitiet) {
       sellerAddressLine: "Số nhà 540, đường 3/2, tổ 8, Phường Tích Lương, Tỉnh Thái Nguyên, Việt Nam",
       sellerPhoneNumber: "0916747401",
       sellerEmail: "lehoan722@gmail.com",
-      sellerBankAccount: "",
-      sellerBankName: ""
+      sellerBankAccount: "2662222333",
+      sellerBankName: "Techcombank"
     },
     cs2: {
       sellerLegalName: "NGUYỄN ÁNH TUYẾT",
@@ -26,8 +26,8 @@ function taoDuLieuHoaDon(hoadon, chitiet) {
       sellerAddressLine: "Số 561, Tổ 11, Phường Phan Đình Phùng, Tỉnh Thái Nguyên, Việt Nam",
       sellerPhoneNumber: "0763424342",
       sellerEmail: "nguyenanhtuyet140175@gmail.com",
-      sellerBankAccount: "",
-      sellerBankName: ""
+      sellerBankAccount: "2552222333",
+      sellerBankName: "Techcombank"
     }
   };
 
