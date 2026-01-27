2dd3088a0eb33f6a7f7edfc5582f0d906ad68136
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2026-01-27 12:53:19 +0700

    Update suadmhanghoa.js

--- scripts/suadmhanghoa.js
+++ scripts/suadmhanghoa.js
@@ -256,7 +256,6 @@ async function fetchRowsByFilterFromDmHangHoa(colname, filterValue, createdAtRan
 function renderColSelect() {
   let html = `<option value="" selected disabled>-- Chọn mục cần ghi --</option>` +
     COLS
-      .filter(c => c.name !== "masp")
       .map(c => `<option value="${c.name}">${c.label}</option>`)
       .join("");
 
@@ -265,6 +264,7 @@ function renderColSelect() {
     colSelect.innerHTML = html;
   }
 }
+
 
 // ==== Table Handsontable (chỉ gồm masp, cột cần sửa, trạng thái) ====
 let hot;
