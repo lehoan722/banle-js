0120f436d5fb871f4d0f8a00ff57319d4356e307
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2026-04-15 10:32:27 +0700

    Update hoadonCcn.js

--- scripts/services/hoadonCcn.js
+++ scripts/services/hoadonCcn.js
@@ -1,7 +1,15 @@
 // scripts/services/hoadonCcn.js
-
 import { luuHoaDonccn1v2 } from '../luuhoadon.js';
 
-export async function saveHoaDonCCN(ctx) {
+export async function saveHoaDonCCN(ctx = {}) {
+  console.log("🚚 saveHoaDonCCN chạy", {
+    sohd: ctx?.sohd || "",
+    isEdit: !!ctx?.isEdit,
+    isCCN: !!ctx?.isCCN
+  });
+
   return await luuHoaDonccn1v2();
 }
+
+// expose để HTML/F2/auto-save gọi qua entry mới
+window.saveHoaDonCCN = saveHoaDonCCN;
