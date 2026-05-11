bf108e3d7b4f03eee1fc0cb1b77ef8ae070686b1
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2026-05-11 23:17:10 +0700

    Refactor Zalo token handler to use authorization code

--- api/zalo/get-token.js
+++ api/zalo/get-token.js
@@ -1,35 +1,34 @@
 export default async function handler(req, res) {
-
   try {
+    const code = req.query.code;
 
-    const appId = process.env.ZALO_APP_ID;
-    const secretKey = process.env.ZALO_OA_SECRET_KEY;
+    if (!code) {
+      return res.status(400).json({
+        ok: false,
+        error: "Thiếu code. Hãy gọi /api/zalo/get-token?code=AUTHORIZATION_CODE"
+      });
+    }
 
-    const response = await fetch(
-      "https://oauth.zaloapp.com/v4/oa/access_token",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          secret_key: secretKey
-        },
-        body: new URLSearchParams({
-          app_id: appId,
-          grant_type: "client_credentials"
-        })
-      }
-    );
+    const response = await fetch("https://oauth.zaloapp.com/v4/oa/access_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        secret_key: process.env.ZALO_OA_SECRET_KEY
+      },
+      body: new URLSearchParams({
+        code,
+        app_id: process.env.ZALO_APP_ID,
+        grant_type: "authorization_code"
+      })
+    });
 
     const data = await response.json();
-
     return res.status(200).json(data);
 
   } catch (err) {
-
     return res.status(500).json({
+      ok: false,
       error: err.message
     });
-
   }
-
 }
