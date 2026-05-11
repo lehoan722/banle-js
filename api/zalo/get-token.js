888da356ab8df56bc244623b7ca5c5aaf68b3536
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2026-05-11 22:56:31 +0700

    Add Zalo token retrieval handler

---
+++ api/zalo/get-token.js
@@ -0,0 +1,33 @@
+export default async function handler(req, res) {
+  try {
+
+    const appId = process.env.ZALO_APP_ID;
+    const secretKey = process.env.ZALO_APP_SECRET;
+
+    const response = await fetch(
+      "https://oauth.zaloapp.com/v4/oa/access_token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          secret_key: secretKey
+        },
+        body: new URLSearchParams({
+          app_id: appId,
+          grant_type: "client_credentials"
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    return res.status(200).json(data);
+
+  } catch (err) {
+
+    return res.status(500).json({
+      error: err.message
+    });
+
+  }
+}
