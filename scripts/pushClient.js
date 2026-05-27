5f414f1bc06f9109b0ea81d5929dc573b71efb96
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2026-05-27 17:02:08 +0700

    Update pushClient.js

--- scripts/pushClient.js
+++ scripts/pushClient.js
@@ -51,6 +51,7 @@ export async function registerPushNotifications({
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
+                action: 'save',
                 manv,
                 diadiem,
                 role,
