0d63f970e5022cc8c30f66056e19aa6f9466b2d4
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2026-06-29 17:35:53 +0700

    Add cafe_config.js with schema and table definitions

---
+++ cafe/scripts/cafe_config.js
@@ -0,0 +1,10 @@
+export const CAFE_SCHEMA = "cafe";
+
+export const CAFE_TABLES = {
+  KHUVUC: "cafe_khuvuc",
+  BAN: "cafe_ban",
+  HANGHOA: "cafe_hanghoa",
+  NHOMHANG: "cafe_nhomhang",
+  HOADON: "cafe_hoadon",
+  HOADON_CT: "cafe_hoadon_ct",
+};
