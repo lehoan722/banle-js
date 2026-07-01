919b476c1d4534117926338a88251fc9de03c6f9
lehoan722 <154224049+lehoan722@users.noreply.github.com>
2026-07-01 21:39:21 +0700

    Refactor Supabase client to use window object
    Update Supabase client initialization to use window object for global access and enable session persistence.

--- cafe/scripts/cafe_supabaseClient.js
+++ cafe/scripts/cafe_supabaseClient.js
@@ -1,25 +1,18 @@
+
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.48.0/+esm";
 
-const SUPABASE_URL =
-  "https://rddjrmbyftlcvrgzlyby.supabase.co";
+const SUPABASE_URL = "https://rddjrmbyftlcvrgzlyby.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZGpybWJ5ZnRsY3ZyZ3pseWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NjU4MDQsImV4cCI6MjA2MjM0MTgwNH0.-0xtqxn6b9OBz4unTTvJ4klxizWhHa1iSuYGm7cOYTM";
 
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkZGpybWJ5ZnRsY3ZyZ3pseWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3NjU4MDQsImV4cCI6MjA2MjM0MTgwNH0.-0xtqxn6b9OBz4unTTvJ4klxizWhHa1iSuYGm7cOYTM";
-
-export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY,
-  {
+if (!window.supabase || !window.supabase.auth) {
+  window.supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
     auth: {
-      persistSession: false,
-      autoRefreshToken: false,
-      detectSessionInUrl: false,
+      persistSession: true,
+      storage: localStorage,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
     },
+  });
+}
 
-    realtime: {
-      params: {
-        eventsPerSecond: 10,
-      },
-    },
-  }
-);
+export const supabase = window.supabase;
